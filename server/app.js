990392d2f7b574d39870bf5ecbb600d46102aaba
Youssef Abdelhady <youssef.abdelhady1@marist.edu>
2022-03-25 10:57:39 -0400

    Update

--- server/app.js
+++ server/app.js
@@ -5,4 +5,17 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views' })
 })
 
+app.get('/page1', function (req,res) {
+    res.sendFile('page1.html', {root:'./client/views' })
+})
+
 app.listen(1337, () => console.log('Marist Chatter listening on port1337!'));
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+app.use(express.static('client/public'));
